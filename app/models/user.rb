674cc475ec754707bc0707c06142ebc7fd736aac
javieku <javieku@gmail.com>
2018-01-30 22:01:23 +0100

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
   class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save   :downcase_email
     before_create :create_activation_digest
 
@@ -42,17 +42,6 @@
       update_attribute(:remember_digest, nil)
     end
 
-    # Converts email to all lower-case.
-    def downcase_email
-      self.email = email.downcase
-    end
-
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
-
     # Activates an account.
     def activate
       update_attribute(:activated,    true)
@@ -63,4 +52,33 @@
     def send_activation_email
       UserMailer.account_activation(self).deliver_now
     end
-  end
+
+      # Sets the password reset attributes.
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
+    end
+
+    # Sends password reset email.
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+    end
+    
+    # Returns true if a password reset has expired.
+    def password_reset_expired?
+      reset_sent_at < 2.hours.ago
+    end
+
+private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+end
