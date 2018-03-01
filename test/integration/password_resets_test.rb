e83d7e22c4980a9b0b744cbd649fb1e0d62adcb3
javieku <javieku@gmail.com>
2018-03-01 22:38:37 +0100

    Misc

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -74,6 +74,6 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
                             password_confirmation: "foobar" } }
     assert_response :redirect
     follow_redirect!
-    assert_match /expired/, response.body
+    assert_match /expired/i, response.body
   end
 end
